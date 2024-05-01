355856b3ad8e4a7d42bb521689e81058e695c59e
Ville Halldin <villehalldin05@gmail.com>
2024-05-01 21:10:25 +0200

    yatzyfying

--- yatzy/js/app.js
+++ yatzy/js/app.js
@@ -1,14 +1,14 @@
+const rollingTimeMilliseconds = 2000;
+const rollingSpeedMilliseconds = 50;
 var rollButton = document.getElementById('roll-button');
 var hasStopped = false;
+var storedDiceValues = [];
 
 rollButton.addEventListener('click', rollDice);
 
 function rollDice() {
-    var diceElements = document.querySelectorAll(".dice");
-    var diceValues = [];
-    var rollingTimeMilliseconds = 2000;
-    var rollingSpeedMilliseconds = 50;
     var rollingDice;
+    var dice = getDice(document.querySelectorAll(".dice-checkbox"));
 
     rollButton.disabled = true;
 
@@ -17,17 +17,54 @@ function rollDice() {
             clearInterval(rollingDice);
             rollingDice = null;
             rollButton.disabled = false;
+            storedDiceValues = [];
+
+            dice.forEach(function(dice) {
+                storedDiceValues.push(dice.value);
+            });
         }
     }, rollingTimeMilliseconds);
 
     rollingDice = setInterval(function() {
-        var i = 0;
-
-        diceElements.forEach(function(node) {
-            var randomNumber = Math.floor(Math.random() * 6) + 1;
-            diceValues[i] = randomNumber;
-            i++;
-            node.src = 'images/dice-' + randomNumber + '.png'
+        dice.forEach(function(dice) {
+            if (!dice.checkbox.checked){
+                var randomNumber = Math.floor(Math.random() * 6) + 1;
+                dice.value = randomNumber;
+                dice.image.src = 'images/dice-' + randomNumber + '.png'
+            }
         });
     }, rollingSpeedMilliseconds);
+}
+
+class Dice {
+    constructor(checkbox, image, value) {
+        this.checkbox = checkbox;
+        this.image = image;
+        this.value = value;
+    }
+}
+
+function getDice(diceElements) {
+    var dice = [];
+
+    for (let i = 0; i < diceElements.length; i++) {
+        var image = findLableForElement(diceElements[i]).getElementsByTagName("img")[0];
+
+        console.log(image);
+         
+        dice.push(new Dice(diceElements[i], image, 1))
+    }
+
+    return dice;
+}
+
+function findLableForElement(element) {
+    var idVal = element.id;
+    labels = document.getElementsByTagName("label");
+
+    for (let i = 0; i < labels.length; i++) {
+        if (labels[i].htmlFor == idVal) {
+            return labels[i];
+        }
+    }
 }
