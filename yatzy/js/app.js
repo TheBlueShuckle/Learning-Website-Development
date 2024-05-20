dc7c71089595d66e40e7a3439ef2384052b2a373
TheBlueShuckle <villehalldin05@gmail.com>
2024-05-20 14:35:06 +0200

    Fix newly created bug

--- yatzy/js/app.js
+++ yatzy/js/app.js
@@ -312,29 +312,24 @@ function updateScoreboardValue(id, value) {
     document.getElementById(id).innerHTML = value;
 }
 
-function initiatePlayers() {    
+function initiatePlayers() {
+    player1 = new Player(null);
+    player2 = new Player(null);
+    
     while (player1.name === undefined || player1.name === null || player1.name === "") {
-        player1 = new Player(assignPlayerNames());
+        let name = prompt("Enter player 1's name: ");
+        player1.name = name.length > 12 ? name.slice(0, 12) : name;
     }
     
     while (player2.name === undefined || player2.name === null || player2.name === "") {
-        player2 = new Player(assignPlayerNames());
+        let name = prompt("Enter player 2's name: ");
+        player2.name = name.length > 12 ? name.slice(0, 12) : name;
     }
     
     document.getElementById('p1-name').innerHTML = player1.name;
     document.getElementById('p2-name').innerHTML = player2.name;
 
     changeCurrentTurnText();
-}
-
-function assignPlayerNames() {
-    let name = prompt("Enter player 2's name: ");
-
-    if (name.length > 12) {
-        name = name.slice(0, 12);
-    }
-
-    return name;
 }
 
 function lockPickedCombinations(player) {
